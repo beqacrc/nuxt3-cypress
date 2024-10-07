cbb16d1ce208c10491a42577051c0f6e6ce34ae9
Beka Chkhaidze <bchkhaidze@crocobet.com>
2024-10-07 18:27:38 +0400

    initial

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const model = ref(0);
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-100">
+    <Counter v-model="model" />
+  </div>
+</template>
---
+++ components/Counter.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+interface Props {
+  min?: number;
+  max?: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  min: -10,
+  max: 10,
+});
+
+const modelValue = defineModel<number>({
+  required: true,
+});
+
+const increment = () => {
+  if (modelValue.value < (props.max ?? Infinity)) {
+    modelValue.value++;
+  }
+};
+
+const decrement = () => {
+  if (modelValue.value > (props.min ?? -Infinity)) {
+    modelValue.value--;
+  }
+};
+</script>
+
+<template>
+  <div class="flex items-center space-x-4">
+    <button
+      @click="decrement"
+      :disabled="modelValue <= min"
+      class="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 disabled:opacity-50"
+    >
+      -
+    </button>
+    <span class="text-xl">{{ modelValue }}</span>
+    <button
+      @click="increment"
+      :disabled="modelValue >= max"
+      class="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 disabled:opacity-50"
+    >
+      +
+    </button>
+  </div>
+</template>
